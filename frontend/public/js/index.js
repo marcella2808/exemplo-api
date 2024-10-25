496e55f28f766f1f5741d3b7b977d05e1cc2629f
marcella2808 <marcella.carneiro@a.fecaf.com.br>
2024-10-25 15:36:56 -0300

    aprimorando o frontend

---
+++ frontend/public/js/index.js
@@ -0,0 +1,7 @@
+document.getElementById('cadastroBtn').onclick = () => {
+    window.location.href = 'http://localhost:3000/clientes/cadastro';
+};
+
+document.getElementById('listaBtn').onclick = () => {
+    window.location.href = 'http://localhost:3000/clientes/lista';
+};

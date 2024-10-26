245d638960ea29c4fae7bc07cdaa28c72314eb87
marcella2808 <marcella.carneiro@a.fecaf.com.br>
2024-10-25 21:49:42 -0300

    melhorando tratamento de erros no backend

--- backend/controllers/clienteController.js
+++ backend/controllers/clienteController.js
@@ -21,8 +21,8 @@ exports.getClienteById = async (req, res) => {
             return res.status(404).json({ message: "Cliente não encontrado." });
         }
         return res.status(200).json(cliente);
-    } catch (err) {
-        return res.status(500).json({ message: "Erro ao buscar cliente.", error: err });
+    } catch (e) {
+        return res.status(500).json({ message: "Erro ao buscar cliente.", error: e });
     }
 };
 
@@ -30,8 +30,8 @@ exports.getClientes = async (req, res) => {
     try {
         const clientes = await Cliente.find();
         return res.status(200).json(clientes);
-    } catch {
-        return res.status(404);
+    } catch(e) {
+        return res.status(500).json({ message: "Erro ao buscar clientes.", error: e });
     }
 };
 
@@ -41,15 +41,18 @@ exports.createCliente = async (req, res) => {
         const newCliente = await Cliente.create(cliente);
         return res.status(201).json(newCliente);
     } catch (e) {
+        if (e.code === 11000) {
+            return res.status(400).json({ message: "Erro: e-mail, CPF ou telefone já existentes." });
+        }
         return res.status(500).json({ message: "Erro ao cadastrar cliente.", error: e });
     }
 };
 
 exports.updateCliente = async (req, res) => {
     const { id } = req.params;
-    const updates = req.body;
+    const data = req.body;
     try {
-        const updatedCliente = await Cliente.findByIdAndUpdate(id, updates, {
+        const updatedCliente = await Cliente.findByIdAndUpdate(id, data, {
             new: true,
             runValidators: true,
         });
@@ -58,6 +61,9 @@ exports.updateCliente = async (req, res) => {
         }
         return res.status(200).json(updatedCliente);
     } catch (e) {
+        if (e.code === 11000) {
+            return res.status(400).json({ message: "Erro: e-mail, CPF ou telefone já existentes." });
+        }
         return res.status(400).json({ message: "Erro ao atualizar o cliente.", error: e });
     }
 };
